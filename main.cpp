79bb9d3d707e53faea0e3697cc5308293c5c2915
Pitágoras <alves.pitagoras@gmail.com>
2016-03-19 17:04:36 -0300

    Main Loop

--- main.cpp
+++ main.cpp
@@ -13,32 +13,75 @@ int main(int argc, char **argv){
 	cout << mergePair(a, res1) << "\n" << mergePair(b, res2) << "\n";*/
 
 	//TO TO: entrada de dados. Entrada provisoria:
+	int oldNSegments;
 	int nSegments = 4;
-	string segments[nSegments] = {"abc", "cde", "efg", "ghij"};
+	string segments[nSegments] = {"abc", "cde", "efg", "ghij"}, oldSegments*;
 
 	//matriz com cada um dos segmentos comparado com os outros
 	//a matriz não possui os elementos que estão na diagonal
 	//nem os elementos acima da diagonal
 	//(por motivos de economia de memoria)
-	StringPair** matrix = new StringPair*[nSegments];
-	for(int i = 0; i < nSegments; i++){
-		int length = i;
-		matrix[i] = new StringPair[length];
-		cout << "line " << i << " has " << length << " comparisons\n";
-	}
+	StringPair** oldMatrix = null;
+	StringPair** matrix;
 
-	//inicialização de cada comparação
-	//ATENÇÃO: A iteração sobre a matriz deve obedecer este formato abaixo
-	//	 			 ou vão ocorrer falhas de segmentação
-	int n = 0;
-	for(int i = 1; i < nSegments; i++){
-		for(int j = 0; j < i; j++){
-			matrix[i][j] = StringPair(segments[i],segments[j]);
-			//matrix[i][j].calcResult();
-			n++;
+	while(nSegments > 1){
+		matrix = new StringPair*[nSegments];
+		for(int i = 0; i < nSegments; i++){
+			int length = i;
+			matrix[i] = new StringPair[length];
+			cout << "line " << i << " has " << length << " comparisons\n";
 		}
+
+		//inicialização de cada comparação
+		//ATENÇÃO: A iteração sobre a matriz deve obedecer este formato abaixo
+		//	 			 ou vão ocorrer falhas de segmentação
+		int n = 0;
+		int xMax = 1, yMax = 0;
+		for(int i = 1; i < nSegments; i++){
+			for(int j = 0; j < i; j++){
+				matrix[i][j] = StringPair(segments[i],segments[j]);
+				//matrix[i][j].calcResult();
+				if(matrix[i][j].result.module > 0){
+					cout << "Merging -" << segments[i] << "- with -" << segments[j] << "- results in ->\n" << matrix[i][j].result.result <<"\n";
+				}
+				if(matrix[i][j].result.module > matrix[xMax][yMax].result.module){
+					xMax = i;
+					yMax = j;
+				}
+				n++;
+			}
+		}
+
+		//yMax sempre vem antes de xMax, a matriz garante isso!
+		StringPair bestMerge = matrix[xMax][yMax];
+		cout << "--------------------\nThe best merge in the iteration is:\n ";
+		cout << "Merging -" << segments[xMax] << "- with -" << segments[yMax] << "- results in ->\n" << bestMerge.result <<"\n";
+
+		String* newSegments = new newSegments[nSegments-1];
+		for(int i = 0; i < yMax; i++){
+			newSegments[i] = segments[i];
+		}
+		newSegments[yMax] = bestMerge.result;
+
+		for(int i = yMax+1; i < xMax; i++){
+			newSegments[i] = segments[i];
+		}
+		for(int i = xMax+1; i < nSegments; i++){
+			newSegments[i-1] = segments[i];
+		}
+
+		StringPair* oldSegments = segments;
+		segments = newSegments;
+
+		oldNSegments = nSegments;
+		nSegments -= 1;
+
+		oldMatrix = matrix;
+
+		cout << n << " different pairs on the matrix\n";
 	}
-	cout << n << " different pairs on the matrix\n";
+	cout << segments[0];
+
 
 
 	return 0;
