211f0c098a382d7faabc769f07a8987f94465a6b
Pitágoras <alves.pitagoras@gmail.com>
2016-03-19 15:19:27 -0300

    Matrix

--- StringPair.h
+++ StringPair.h
@@ -4,53 +4,65 @@
 
 using namespace std;
 
-struct StringPair{
+struct CompareResult{
+	CompareResult(){
+		module = -1;
+		initial = false;
+	}
+	CompareResult(int m, bool i){
+		module = m;
+		initial = i;
+	}
+	int module;
+	bool initial;
+	string result;
+};
+
+class StringPair{
+public:
+	string x;
+	string y;
+	bool compared;
+	CompareResult result;
+	StringPair(){
+		x = "";
+		y = "";
+		compared = false;
+	}
 	StringPair(string a, string b){
 		x = a;
 		y = b;
+		compared = false;
 	}
-	string x;
-	string y;
+
+	string printPair(){
+		return x + ", " + y;
+	}
+
+	void calcResult(){
+			if(!compared){
+					//result = calcularCompareResult(StringPair(this->x, this->y));
+					compared = true;
+			}
+	}
 };
 
-	inline bool operator==(const StringPair& first, const StringPair& second){
-		if((first.x.compare(second.x) == 0) && (first.y.compare(second.y) == 0)){
-			return true;
-		}else if((first.x.compare(second.y) == 0) && (first.y.compare(second.x) == 0)){
-			return true;
-		}
-
-		return false;
+inline bool operator==(const StringPair& first, const StringPair& second){
+	if((first.x.compare(second.x) == 0) && (first.y.compare(second.y) == 0)){
+		return true;
+	}else if((first.x.compare(second.y) == 0) && (first.y.compare(second.x) == 0)){
+		return true;
 	}
-
-	inline bool operator!=(const StringPair& first, const StringPair& second){
-		return (!(first == second));
-	}
-
-	struct CompareResult{
-		CompareResult(int m, bool i){
-			module = m;
-			initial = i;
-		}
-		int module;
-		bool initial;
-	};
-
-string mergePair(StringPair a, CompareResult comp){
-	string result;
-	string cuttedY;
-	if(comp.initial){
-		cuttedY = a.y.substr(0, a.y.size() - comp.module);
-		result = cuttedY;
-		result.append(a.x);
-	}else{
-		cuttedY =  a.y.substr(comp.module,  a.y.size() - comp.module);
-		result = a.x;
-		result.append(cuttedY);
-	}
-
-	return result;
+	return false;
 }
+
+inline bool operator!=(const StringPair& first, const StringPair& second){
+	return (!(first == second));
+}
+
+
+
+
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
+#include <string>
 #include "StringPair.h"
 
 using namespace std;
 
 int main(int argc, char **argv){
-	StringPair a("abcde", "defgh");
+	/*StringPair a("abcde", "defgh");
 	StringPair b("defgh", "abcde");
 
 	CompareResult res1(2, false), res2(2, true);
-	cout << mergePair(a, res1) << "\n" << mergePair(b, res2) << "\n";
+	cout << mergePair(a, res1) << "\n" << mergePair(b, res2) << "\n";*/
+
+	//TO TO: entrada de dados. Entrada provisoria:
+	int nSegments = 4;
+	string segments[nSegments] = {"abc", "cde", "efg", "ghij"};
+
+	//matriz com cada um dos segmentos comparado com os outros
+	//a matriz não possui os elementos que estão na diagonal
+	//nem os elementos acima da diagonal
+	//(por motivos de economia de memoria)
+	StringPair** matrix = new StringPair*[nSegments];
+	for(int i = 0; i < nSegments; i++){
+		int length = i;
+		matrix[i] = new StringPair[length];
+		cout << "line " << i << " has " << length << " comparisons\n";
+	}
+
+	//inicialização de cada comparação
+	//ATENÇÃO: A iteração sobre a matriz deve obedecer este formato abaixo
+	//	 			 ou vão ocorrer falhas de segmentação
+	int n = 0;
+	for(int i = 1; i < nSegments; i++){
+		for(int j = 0; j < i; j++){
+			matrix[i][j] = StringPair(segments[i],segments[j]);
+			//matrix[i][j].calcResult();
+			n++;
+		}
+	}
+	cout << n << " different pairs on the matrix\n";
 
 
 	return 0;
